a1da8c6c1e3cbdfa403ec07560ae1d65affce835
Ali Dahshan <alidahshan911@gmail.com>
2024-07-20 00:54:59 +0300

    first commit

--- android/task 3/mainscreen.dart
+++ android/task 3/mainscreen.dart
@@ -9,7 +9,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'hello',
+      title: 'task 3',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
